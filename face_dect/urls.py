409e36cc8673ef448ea566193f9b6703af724b59
umar.ali <umaru.alia@gmail.com>
2020-01-12 03:47:46 -0800

    chat bot

--- face_dect/urls.py
+++ face_dect/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),#indez url
     url(r'^info/$', views.info, name='info'),#result url
     url(r'^history/$', views.getHistory, name='history'),#history url
+    url(r'^chat/$', views.chat, name='chat'),
 ]
